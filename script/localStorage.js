05a52954541a316b8f2a03e4a5567106c447b52a
baranovdv <baranov.d.v.1988@gmail.com>
2023-05-02 00:51:36 +0700

    refactor: refactor js modules

--- script/localStorage.js
+++ script/localStorage.js
@@ -1,9 +1,7 @@
-export function setLocalStorage(Obj) {
-  localStorage.setItem('lang', Obj.output.lang);
+export function setLocalStorage(name, item) {
+  localStorage.setItem(name, item);
 }
 
-export function getLocalStorage(Obj) {
-  if (localStorage.getItem('lang')) {
-    Obj.output.lang = localStorage.getItem('lang');
-  }
+export function getLocalStorage(name) {
+      return localStorage.getItem(name);
 }
